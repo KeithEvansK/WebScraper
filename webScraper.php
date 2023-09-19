c335a814bebae4ed41f0655da81e57ad70af334c
KeithEvansK <99915276+KeithEvansK@users.noreply.github.com>
2023-09-19 06:25:37 -0400

    Update and rename spidertest (1).php to webScraper.php

--- webScraper.php
+++ webScraper.php
@@ -2,16 +2,20 @@
 <html>
 <body>
 
-<h1>PHP test</h1>
+<h1> Web Scraper </h1>
 
 <?php
+
+//import html dom parser.
 include_once('simple_html_dom.php'); // http://simplehtmldom.sourceforge.net/
 
 $html = new simple_html_dom();
-$html->load_file('http://technobuffalo.com/');
+
+//load the html page from this given url.
+$html->load_file('https://youtube.com/');
 
 
-
+//for each ahref found, display them in a list.
 foreach($html->find('a') as $element)
        echo $element , "\n" , $element->href ,"\n"; 
 
@@ -19,15 +23,10 @@ echo $html;
 
 foreach($html->find('img') as $element) {
     echo $element->src, "\n";
-    
- //   Thumbnail($element, "temp.png");
-    
-   // echo '<img src="temp.png" />';
-    
 
 }
 
-
+//Handle elements if they are an image.
 function Thumbnail($url, $filename, $width = 150, $height = true) {
 
  // download and create gd image
